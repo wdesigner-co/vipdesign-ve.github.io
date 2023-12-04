66404bb9bf42f03e0ce45391480fd4caebe6b306
Alex Col | WDesigner <102479000+wdesigner-co@users.noreply.github.com>
2023-12-04 12:54:36 -0500

    Update registrarmail.php

--- registrarmail.php
+++ registrarmail.php
@@ -1,19 +1,33 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   // Obtener los datos del formulario
-  $name = $_POST['nombre'];
-  $email = $_POST['email'];
+  $nombre = $_POST['nombre'];
+  $correo = $_POST['correo'];
+  
+  // Validar que los campos no estén vacíos
+  if (empty($nombre) || empty($correo)) {
+    echo "Por favor, complete todos los campos";
+    exit;
+  }
 
   // Construir el contenido del correo electrónico
-  $to = 'vipdesign.venezuela@gmail.com'; 
-  $subject = 'Formulario de Registro';
+  $destinatario = 'vipdesign.venezuela@gmail.com'; 
+  $asunto = 'Formulario de Registro';
   $mensaje = "Se ha recibido un nuevo mensaje de la Landing Page de Identidad Visual:\n\n";
-  $body = "Nombre: $name\n";
-  $body .= "Correo: $email\n";
-  $body .= "Mensaje: Contactar a cliente";
+  $mensaje .= "Nombre: $nombre\n";
+  $mensaje .= "Correo: $correo\n";
+  $mensaje .= "Mensaje: Contactar a cliente";
+
+  // Crear las cabeceras del correo electrónico
+  $cabeceras = "From: vipdesign.venezuela@gmail.com\r\n";
+  $cabeceras .= "Reply-To: vipdesign.venezuela@gmail.com\r\n";
+  $cabeceras .= "X-Mailer: PHP/" . phpversion();
 
   // Enviar el correo electrónico
-  mail($to, $subject, $mensaje, $body);
-
+  if (mail($destinatario, $asunto, $mensaje, $cabeceras)) {
+    echo "El correo se ha enviado correctamente";
+  } else {
+    echo "Hubo un error al enviar el correo";
+  }
 }
 ?>
